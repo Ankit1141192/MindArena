fa6052a5fbf1235bd0477a5baab105325fc066b2
akshay Poonia <akshay12345poonia@gmail.com>
2025-08-18 18:11:47 +0530

    flappy

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import FlappyBird from './components/FlappyBird'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-game-bg flex items-center justify-center p-5">
+      <FlappyBird />
+    </div>
+  )
+}
+
+export default App
